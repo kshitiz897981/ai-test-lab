6f2c4761c6a1ad472ff7807b9b314f5c13388e29
Kshitiz Varshney <kshitizvarshney4@gmail.com>
2026-09-03 15:34:08 +0530

    error limit ui improved

--- backend/src/controllers/project.controller.js
+++ backend/src/controllers/project.controller.js
@@ -1,5 +1,5 @@
 import projectModel from "../models/project.model.js";
-import testCaseModel from "../models/testCase.model.js";
+import testCaseModel from "../models/testCase.model.js
 import testResultModel from "../models/testResult.model.js";
 
 
@@ -178,42 +178,8 @@ export const updateProject = async (req, res) => {
     }
 };
 
-export const getProjectTests = async (req, res) => {
-    try {
-        const { id } = req.params;
 
-        const project = await projectModel.findById(id);
 
-        if (!project) {
-            return res.status(404).json({
-                message: "Project not found"
-            });
-        }
-
-        const testCases = await testCaseModel.find({
-            project: project._id
-        });
-
-        const results = await testResultModel.find({
-            project: project._id
-        }).sort({
-            createdAt: -1
-        });
-
-        return res.status(200).json({
-            testCases,
-            results
-        });
-
-    } catch (error) {
-        console.error(error);
-
-        return res.status(500).json({
-            message: "Failed to load project tests",
-            error: error.message
-        });
-    }
-};
 
 export const getProjectAnalytics = async (req, res) => {
     try {
